0e906261a62c9b05a6eafc2fd93ed077f86b7f94
Vikkelas <bass89@mail.ru>
2021-09-24 00:38:08 +0300

    final end home work

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,31 +1,62 @@
 const url = 'https://netology-slow-rest.herokuapp.com/poll.php'
 let title = document.getElementById('poll__title')
 let answer = document.getElementById('poll__answers')
+let id
 
-function createRequest(type, url) {
-	let xhr = new XMLHttpRequest()
-	xhr.open(type, url)
-	xhr.responseType = 'json'
-	xhr.send()
-	return xhr
+const firstReq = {
+	type: 'GET',
+	url: url,
+	callback: addSurvey,
+	headerType: 'Content-type',
+	headerValue: 'application/x-www-form-urlencoded',
 }
-let data = createRequest('GET', url)
 
-//
-// xhr.addEventListener('loadend', () => {
-// 	console.log(xhr.response)
-// 	let data = xhr.response.data
-// 	title.innerText = data.title
-// 	for (let i = 0; i < data.answers.length; i++) {
-// 		let creatAnswer = document.createElement('button')
-// 		creatAnswer.classList.add('poll__answer')
-// 		creatAnswer.innerHTML = data.answers[i]
-// 		answer.appendChild(creatAnswer)
-// 	}
+createRequest(firstReq)
 
-// 	answer.addEventListener('click', () => {
-// 		alert('Ваш голос учтен')
-// 		// let idAnswer = data.answers.indexOf(e.target.innerHTML)
-// 		// let idArr = xhr.response.id
-// 	})
-// })
+function createRequest(options = {}) {
+	let xhr = new XMLHttpRequest()
+	xhr.open(options.type, options.url)
+	xhr.setRequestHeader(options.headerType, options.headerValue)
+	xhr.responseType = 'json'
+	xhr.send(options.send)
+	xhr.addEventListener('loadend', () => {
+		options.callback(xhr.response)
+	})
+}
+
+function addSurvey(index) {
+	title.innerText = index.data.title
+	let answers = [...index.data.answers]
+	id = index.id
+	for (let i = 0; i < answers.length; i++) {
+		let button = document.createElement('button')
+		button.classList.add('poll__answer')
+		button.innerHTML = answers[i]
+		answer.appendChild(button)
+	}
+}
+
+answer.addEventListener('click', e => {
+	alert('спасибо ваш голос учтен!')
+	let clickAnswer = e.target.textContent
+	let secondReq = {
+		type: 'POST',
+		url: url,
+		callback: addSurvey,
+		headerType: 'Content-type',
+		headerValue: 'application/x-www-form-urlencoded',
+		send: 'vote=' + id + '&' + 'answer=' + clickAnswer,
+		callback: getStatistics,
+	}
+	createRequest(secondReq)
+})
+
+function getStatistics(index) {
+	answer.innerHTML = ''
+	const answerStatics = [...index.stat]
+	answerStatics.forEach(element => {
+		const statisticsBox = document.createElement('div')
+		statisticsBox.textContent = element.answer + ': ' + element.votes
+		answer.appendChild(statisticsBox)
+	})
+}
