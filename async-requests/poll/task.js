507834540c577836cbd9ddcee21ac1cac81231bf
Vikkelas <bass89@mail.ru>
2021-09-23 15:30:36 +0300

    234

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,25 +1,31 @@
 const url = 'https://netology-slow-rest.herokuapp.com/poll.php'
 let title = document.getElementById('poll__title')
 let answer = document.getElementById('poll__answers')
-let xhr = new XMLHttpRequest()
 
-xhr.open('GET', url)
-xhr.responseType = 'json'
-xhr.send()
-xhr.addEventListener('loadend', () => {
-	console.log(xhr.response)
-	let data = xhr.response.data
-	title.innerText = data.title
-	for (let i = 0; i < data.answers.length; i++) {
-		let creatAnswer = document.createElement('button')
-		creatAnswer.classList.add('poll__answer')
-		creatAnswer.innerHTML = data.answers[i]
-		answer.appendChild(creatAnswer)
-	}
+function createRequest(type, url) {
+	let xhr = new XMLHttpRequest()
+	xhr.open(type, url)
+	xhr.responseType = 'json'
+	xhr.send()
+	return xhr
+}
+let data = createRequest('GET', url)
 
-	answer.addEventListener('click', () => {
-		alert('Ваш голос учтен')
-		// let idAnswer = data.answers.indexOf(e.target.innerHTML)
-		// let idArr = xhr.response.id
-	})
-})
+//
+// xhr.addEventListener('loadend', () => {
+// 	console.log(xhr.response)
+// 	let data = xhr.response.data
+// 	title.innerText = data.title
+// 	for (let i = 0; i < data.answers.length; i++) {
+// 		let creatAnswer = document.createElement('button')
+// 		creatAnswer.classList.add('poll__answer')
+// 		creatAnswer.innerHTML = data.answers[i]
+// 		answer.appendChild(creatAnswer)
+// 	}
+
+// 	answer.addEventListener('click', () => {
+// 		alert('Ваш голос учтен')
+// 		// let idAnswer = data.answers.indexOf(e.target.innerHTML)
+// 		// let idArr = xhr.response.id
+// 	})
+// })
